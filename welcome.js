f5aa7f56be68ac044bad6c31caf3c2262e7c5241
simbachristian07-star <simbachristian07@gmail.com>
2026-08-10 23:25:28 +0300

    Rename supabase variable to supabaseClient

--- welcome.js
+++ welcome.js
@@ -1,6 +1,6 @@
 const SUPABASE_URL = "https://fqyckjopcymubjokxwcs.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxeWNram9wY3ltdWJqb2t4d2NzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzODE5NDQsImV4cCI6MjEwMTk1Nzk0NH0.w46whTG31zt2bKAJ-TZYxcZSiOZ7G8fxLvkXDcRNXq0";
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = window.supabase.createClient(...)
 
 window.onload = function () {
 
